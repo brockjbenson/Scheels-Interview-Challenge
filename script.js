3696f2c9a515a7d1fc961ccf085a48fd22903220
Brock Benson <brockjbenson@outlook.com>
2023-03-31 12:46:54 -0500

    functionality for application working

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 $(() => {
-  console.log("In jQuery");
+  $("#run-btn").on("click", displayResults);
+  $("#reset-btn").on("click", resetResults);
 });
+
+const minNum = 1;
+const maxNum = 100;
+const textOne = "SCHEELS";
+const textTwo = ".COM";
+const combinedText = textOne + textTwo;
+
+function displayResults() {
+  $("#output-list").html("");
+
+  for (let i = minNum; i <= maxNum; i++) {
+    let result = "";
+
+    if (i % 3 === 0 && i % 5 === 0) {
+      result = combinedText;
+    } else if (i % 3 === 0) {
+      result = textOne;
+    } else if (i % 5 === 0) {
+      result = textTwo;
+    } else {
+      result = i;
+    }
+    $("#output-list").append(`<li>${result}</li>`);
+  }
+}
+
+function resetResults() {
+  $("#output-list").html("");
+}
