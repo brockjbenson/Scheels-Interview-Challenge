e2da6215d8172c4639acf8f224b7b6b7437c179b
Brock Benson <brockjbenson@outlook.com>
2023-04-02 10:30:12 -0500

    styling

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const maxNum = 100;
 const textOne = "SCHEELS";
 const textTwo = ".COM";
 const combinedText = textOne + textTwo;
+const url =
+  "https://www.scheels.com/?gclid=Cj0KCQjwz6ShBhCMARIsAH9A0qWc2AFPJ9OKAAwqPkEvgLbPChHBF6gyrt1ILmjktadCpt-rXzkOdtwaAplXEALw_wcB";
 
 function displayResults() {
   $("#output-list").html("");
@@ -17,14 +19,21 @@ function displayResults() {
 
     if (i % 3 === 0 && i % 5 === 0) {
       result = combinedText;
+      $("#output-list").append(
+        `<li>
+          <a target="_blank" class="combined" href=${url}>${result}</a>
+        </li>`
+      );
     } else if (i % 3 === 0) {
       result = textOne;
+      $("#output-list").append(`<li class="SCHEELS">${result}</li>`);
     } else if (i % 5 === 0) {
       result = textTwo;
+      $("#output-list").append(`<li class="COM">${result}</li>`);
     } else {
       result = i;
+      $("#output-list").append(`<li class="number">${result}</li>`);
     }
-    $("#output-list").append(`<li>${result}</li>`);
   }
 }
 
